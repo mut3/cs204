7f276fb51fd1a1a6fd58142c0d6ef77386743725
Will Barnwell <wwbarnwell@gmail.com>
2015-10-28 16:18:15 -0400

    added constraints

--- hw7/hw7.sql
+++ hw7/hw7.sql
@@ -2,58 +2,68 @@ DROP TABLE IF EXISTS tblEmployee, tblDepartment, tblDeptLocations, tblProject, t
 
 CREATE TABLE IF NOT EXISTS tblEmployee
 (
-	fldFname data_type(size) constraint_name,
-	fldMinit data_type(size) constraint_name,
-	fldLname data_type(size) constraint_name,
-	pmkSsn data_type(size) constraint_name,
-	fldBdate data_type(size) constraint_name,
-	fldAddress data_type(size) constraint_name,
-	fldSex data_type(size) constraint_name,
-	fldSalary data_type(size) constraint_name,
-	fnkSuperSsn data_type(size) constraint_name,
-	fnkDno data_type(size) constraint_name,
+	fldFname varchar(255),
+	fldMinit varchar(255),
+	fldLname varchar(255),
+	pmkSsn varchar(255) NOT NULL,
+	fldBdate varchar(255),
+	fldAddress varchar(255),
+	fldSex varchar(255),
+	fldSalary varchar(255),
+	fnkSuperSsn varchar(255),
+	fnkDno varchar(255),
+	CONSTRAINT chk_EmployeeSex CHECK (fldSex='M' OR fldSex='F'),
+	CONSTRAINT chk_EmployeeSalary CHECK (fldSalary > 20000),
 	PRIMARY KEY(pmkSsn)
 );
 
 CREATE TABLE IF NOT EXISTS tblDepartment
 (
-	fldDname data_type(size) constraint_name,
-	pmkDnumber data_type(size) constraint_name,
-	fnkMgrSsn data_type(size) constraint_name,
-	fldMgrStartDate data_type(size) constraint_name,
+	fldDname varchar(255),
+	pmkDnumber varchar(255) NOT NULL,
+	fnkMgrSsn varchar(255),
+	fldMgrStartDate varchar(255),
 	PRIMARY KEY(pmkDnumber)
 );
 
 CREATE TABLE IF NOT EXISTS tblDeptLocations
 (
-	fnkDnumber data_type(size) constraint_name,
-	fldDlocation data_type(size) constraint_name,
+	fnkDnumber varchar(255) NOT NULL,
+	fldDlocation varchar(255) NOT NULL,
 	PRIMARY KEY(fnkDnumber, fldDlocation)
 );
 
 CREATE TABLE IF NOT EXISTS tblProject
 (
-	fldPname data_type(size) constraint_name,
-	pmkPnumber data_type(size) constraint_name,
-	fldPlocation data_type(size) constraint_name,
-	fnkDnum data_type(size) constraint_name,
+	fldPname varchar(255),
+	pmkPnumber varchar(255) NOT NULL,
+	fldPlocation varchar(255),
+	fnkDnum varchar(255),
+	CONSTRAINT chk_ProjLocation CHECK (fldPlocation='Bellaire' OR fldPlocation='Sugarland' OR fldPlocation='Houston' OR fldPlocation='Stafford'),
 	PRIMARY KEY(pmkPnumber)
 );
 
 CREATE TABLE IF NOT EXISTS tblWorksOn
 (
-	fnkEssn data_type(size) constraint_name,
-	fnkPno data_type(size) constraint_name,
-	fldHours data_type(size) constraint_name,
+	fnkEssn varchar(255) NOT NULL,
+	fnkPno varchar(255) NOT NULL,
+	fldHours varchar(255),
+	CONSTRAINT chk_WorksOn_Hours CHECK (fldHours >= 5 AND fldHours <= 40),
 	PRIMARY KEY(fnkEssn, fnkPno)
 );
 
-CREATE TABLE IF NOT EXISTS tblEmployee
+CREATE TABLE IF NOT EXISTS tblDependent
 (
-	fnkEssn data_type(size) constraint_name,
-	fldDependentName data_type(size) constraint_name,
-	fldSex data_type(size) constraint_name,
-	fldBdate data_type(size) constraint_name,
-	fldRelationship data_type(size) constraint_name,
+	fnkEssn varchar(255) NOT NULL,
+	fldDependentName varchar(255) NOT NULL,
+	fldSex varchar(255),
+	fldBdate varchar(255),
+	fldRelationship varchar(255),
 	PRIMARY KEY(fnkEssn, fldDependentName)
 );
+
+ALTER TABLE Orders
+ADD CONSTRAINT fk_PerOrders
+FOREIGN KEY (P_Id)
+REFERENCES Persons(P_Id)
+
