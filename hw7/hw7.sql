be7e07918daeb2dee0fcc6693fcf27c10e71249d
Will Barnwell <wwbarnwell@gmail.com>
2015-10-28 16:41:53 -0400

    added foreign key contraints

--- hw7/hw7.sql
+++ hw7/hw7.sql
@@ -3,17 +3,18 @@ DROP TABLE IF EXISTS tblEmployee, tblDepartment, tblDeptLocations, tblProject, t
 CREATE TABLE IF NOT EXISTS tblEmployee
 (
 	fldFname varchar(255),
-	fldMinit varchar(255),
+	fldMinit varchar(1),
 	fldLname varchar(255),
-	pmkSsn varchar(255) NOT NULL,
+	pmkSsn int(9) NOT NULL,
 	fldBdate varchar(255),
 	fldAddress varchar(255),
 	fldSex varchar(255),
 	fldSalary varchar(255),
-	fnkSuperSsn varchar(255),
+	fnkSuperSsn int(9),
 	fnkDno varchar(255),
 	CONSTRAINT chk_EmployeeSex CHECK (fldSex='M' OR fldSex='F'),
 	CONSTRAINT chk_EmployeeSalary CHECK (fldSalary > 20000),
+	CONSTRAINT fk_EmpDno FOREIGN KEY (fnkDno) REFERENCES tblDepartment(pmkDnumber),
 	PRIMARY KEY(pmkSsn)
 );
 
@@ -23,6 +24,7 @@ CREATE TABLE IF NOT EXISTS tblDepartment
 	pmkDnumber varchar(255) NOT NULL,
 	fnkMgrSsn varchar(255),
 	fldMgrStartDate varchar(255),
+	CONSTRAINT fk_DeptMgrSsn FOREIGN KEY (fnkMgrSsn) REFERENCES tblEmployee(pmkSsn),
 	PRIMARY KEY(pmkDnumber)
 );
 
@@ -30,6 +32,7 @@ CREATE TABLE IF NOT EXISTS tblDeptLocations
 (
 	fnkDnumber varchar(255) NOT NULL,
 	fldDlocation varchar(255) NOT NULL,
+	CONSTRAINT fk_DeptLocDnumber FOREIGN KEY (fnkDnumber) REFERENCES tblDepartment(pmkDnumber),
 	PRIMARY KEY(fnkDnumber, fldDlocation)
 );
 
@@ -40,6 +43,7 @@ CREATE TABLE IF NOT EXISTS tblProject
 	fldPlocation varchar(255),
 	fnkDnum varchar(255),
 	CONSTRAINT chk_ProjLocation CHECK (fldPlocation='Bellaire' OR fldPlocation='Sugarland' OR fldPlocation='Houston' OR fldPlocation='Stafford'),
+	CONSTRAINT fk_PrjDnum FOREIGN KEY (fnkDnum) REFERENCES tblDepartment(pmkDnumber),
 	PRIMARY KEY(pmkPnumber)
 );
 
@@ -49,21 +53,21 @@ CREATE TABLE IF NOT EXISTS tblWorksOn
 	fnkPno varchar(255) NOT NULL,
 	fldHours varchar(255),
 	CONSTRAINT chk_WorksOn_Hours CHECK (fldHours >= 5 AND fldHours <= 40),
+	CONSTRAINT fk_WksEssn FOREIGN KEY (fnkEssn) REFERENCES tblEmployee(pmkSsn),
+	CONSTRAINT fk_WksPno FOREIGN KEY (fnkPno) REFERENCES tblProject(pmkPnumber),
 	PRIMARY KEY(fnkEssn, fnkPno)
 );
 
 CREATE TABLE IF NOT EXISTS tblDependent
 (
-	fnkEssn varchar(255) NOT NULL,
+	fnkEssn int(9) NOT NULL,
 	fldDependentName varchar(255) NOT NULL,
-	fldSex varchar(255),
+	fldSex char(1),
 	fldBdate varchar(255),
 	fldRelationship varchar(255),
+	CONSTRAINT fk_DepnEssn FOREIGN KEY (fnkEssn) REFERENCES tblEmployee(pmkSsn),
 	PRIMARY KEY(fnkEssn, fldDependentName)
 );
 
-ALTER TABLE Orders
-ADD CONSTRAINT fk_PerOrders
-FOREIGN KEY (P_Id)
-REFERENCES Persons(P_Id)
+ALTER TABLE tblEmployee ADD CONSTRAINT fk_EmpSuperSsn FOREIGN KEY (fnkSuperSsn) REFERENCES tblEmployee(pmkSsn);
 
