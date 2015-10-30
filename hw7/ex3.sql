34485140fdacdc4b81a46059300ff2dc60f96639
Will Barnwell <wwbarnwell@gmail.com>
2015-10-29 21:16:02 -0400

    f and g work

--- hw7/ex3.sql
+++ hw7/ex3.sql
@@ -10,18 +10,23 @@ INSERT INTO tblWorksOn VALUES (333445555, 30, NULL);
 
 -- create subtable ESSN|count( projects for ESSN)
 
-SELECT fldFname, fldLname FROM tblEmployee JOIN tblWorksOn ON tblEmployee.pmkSsn = tblWorksOn.fnkEssn WHERE (SELECT tblWorksOn.fnkEssn AS 'subSsn', COUNT(tblWorksOn.fnkPno) FROM tblWorksOn GROUP BY subSsn);
+SELECT fldFname, fldLname FROM tblEmployee JOIN (SELECT tblWorksOn.fnkEssn AS refSsn, COUNT(tblWorksOn.fnkPno) AS projCount FROM tblWorksOn GROUP BY tblWorksOn.fnkEssn) ON refSsn = tblEmployee.pmkSsn WHERE projCount = (SELECT COUNT(DISTINCT pmkPnumber) FROM tblProject);
 
 -- delete the information added in the first step
 DELETE FROM tblWorksOn WHERE fnkEssn = 333445555 AND fnkPno IN (1,30);
 
 -- f. Retrieve the names of all employees who do not work on any project.
+DELETE FROM tblWorksOn WHERE fnkEssn = 888665555 AND fnkPno = 20;
 
 SELECT fldFname, fldLname FROM tblEmployee WHERE pmkSsn NOT IN (SELECT DISTINCT fnkEssn FROM tblWorksOn);
 
--- g. For each department, retrieve the department name and the average
--- salary of all employees working in that department.
+INSERT INTO tblWorksOn VALUES (888665555, 20, null);
+
+-- g. For each department, retrieve the department name and the average salary of all employees working in that department.
+SELECT tblDepartment.fldDname, avgSal FROM tblDepartment JOIN (SELECT tblEmployee.fnkDno AS refDno, AVG(fldSalary) AS avgSal FROM tblEmployee GROUP BY tblEmployee.fnkDno) ON tblDepartment.pmkDnumber = refDno;
+
 -- h. Retrieve the average salary of all female employees.
+
 -- i. Find the names and addresses of all employees who work on at least one project located in Houston but whose department has no location in Houston.
 -- j. List the last names of all department managers who have no dependents
 SELECT fldLname FROM tblEmployee WHERE pmkSsn NOT IN (SELECT DISTINCT fnkEssn FROM tblDependent) AND pmkSsn IN (SELECT DISTINCT fnkMgrSsn FROM tblDepartment);
