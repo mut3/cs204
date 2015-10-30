f8803c1ca78b5a076d88ab0f9dd43aadf0b3a114
Will Barnwell <wwbarnwell@gmail.com>
2015-10-30 00:39:58 -0400

    done a

---
+++ hw7/ex5.sql
@@ -0,0 +1,9 @@
+-- a) A department must have at least one employee. Note: Insert a tuple whose dnumber equals 6 before checking this constraint. All the other columns may be null. Make sure to roll back the insertion after checking the constraint. (In order to rollback, include “rollback;” at the end.)
+INSERT INTO tblDepartment VALUES ('BTTF Remembrance', 6, 333445555, '21-October-2015');
+
+SELECT * FROM tblDepartment WHERE tblDepartment.pmkDnumber NOT IN (SELECT DISTINCT fnkDno FROM tblEmployee);
+
+rollback;
+
+-- b) An employee with two or more dependents must receive a minimum salary of $35000.
+-- c) An employee must work on projects for a total of at least 37.5 hours per week.
