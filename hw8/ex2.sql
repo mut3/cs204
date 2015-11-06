fe592248b1dd6f11cebc68ce0df668835cc84c85
Will Barnwell <wwbarnwell@gmail.com>
2015-11-06 01:52:38 -0500

    meh

--- hw8/ex2.sql
+++ hw8/ex2.sql
@@ -2,8 +2,8 @@
  * For each of a, b, and c, execute not only the statement on the view but also an equivalent statement on the base table EMPLOYEE. 
  * For d and e, which do not work, execute each statement on the view and explain briefly why it does not work.
  */
-@runscript tblCreate;
-@runscript tblFill;
+start tblCreate;
+start tblFill;
 
 CREATE VIEW DEPT_SUMMARY (D, C, Total_s, Average_s)
 AS SELECT fnkDno, COUNT (*), SUM (fldSalary), AVG (fldSalary)
