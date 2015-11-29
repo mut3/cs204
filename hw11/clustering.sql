04d94f52976fc2d9a5173fe0294fd9e1708456b6
Will Barnwell <wwbarnwell@gmail.com>
2015-11-29 15:13:37 -0500

    removed inserts for script

--- hw11/clustering.sql
+++ hw11/clustering.sql
@@ -43,30 +43,7 @@ commit;
 CREATE INDEX idxDepartment ON CLUSTER clsDepartment;
 commit;
 -- iii. Insert the tuples shown in the textbook Figure 3.6 (page 72) into the clustered tables. Insert null for the column mgrssn of the Department table, since the table Employee is omitted in this exercise. You can use insert_tuples_dept_locs_projs.sql as is.(Specifically, include the command “start insert_tuples_dept_locs_projs.sql” in the input script.)
-/* Textbook Company database (Figure 3.6)
- * Insert statements listed in the alphabetical order of table 
- * names. Modify this order before inserting the tuples into the 
- * tables. Make any additional necessary modifications.
- */
-
-INSERT INTO tblDepartment VALUES ('Research', 5, NULL, '22-May-78'); 
-INSERT INTO tblDepartment VALUES ('Administration', 4, NULL, '01-Jan-85');
-INSERT INTO tblDepartment VALUES ('Headquarters', 1, NULL, '19-Jun-71'); 
-
-INSERT INTO tblDeptLocations VALUES (1, 'Houston');
-INSERT INTO tblDeptLocations VALUES (4, 'Stafford');
-INSERT INTO tblDeptLocations VALUES (5, 'Bellaire');
-INSERT INTO tblDeptLocations VALUES (5, 'Sugarland');
-INSERT INTO tblDeptLocations VALUES (5, 'Houston');
-
-INSERT INTO tblProject VALUES ('ProductX', 1, 'Bellaire', 5);
-INSERT INTO tblProject VALUES ('ProductY', 2, 'Sugarland', 5);
-INSERT INTO tblProject VALUES ('ProductZ', 3, 'Houston', 5);
-INSERT INTO tblProject VALUES ('Computerization', 10, 'Stafford', 4);
-INSERT INTO tblProject VALUES ('Reorganization', 20, 'Houston', 1);
-INSERT INTO tblProject VALUES ('Newbenefits', 30, 'Stafford', 4);
-
-commit;
+start tblFill.sql;
 
 -- iv. Execute the following queries against the clustered tables: (1) retrieve tuples from each clustered table (i.e., Department, Dept_locations, Project),  Note that, although the files of the three tables have been merged into one, we still see them as separate tables at the logical level.
 SELECT * FROM tblDepartment;
